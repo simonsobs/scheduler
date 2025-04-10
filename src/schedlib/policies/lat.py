123fa5708e992450210b75673151d816a057da11
Michael McCrackan <mikemccrackan@gmail.com>
2025-04-10 10:42:07 -0400

    Add LAT shutter commands (#214)
    * add lat pointing offsets
    
    * add shutter controls

--- src/schedlib/policies/lat.py
+++ src/schedlib/policies/lat.py
@@ -101,12 +101,18 @@ class SchedMode(tel.SchedMode):
 #      return state, 10, ["do something"]
 
 @cmd.operation(name="lat.preamble", duration=0)
-def preamble():
-    return tel.preamble()
+def preamble(open_shutter=False):
+    cmd = tel.preamble()
+    if open_shutter:
+        cmd += ["acu.set_shutter(action='open')"]
+    return cmd
 
 @cmd.operation(name='lat.wrap_up', duration=0)
-def wrap_up(state, block):
-    return tel.wrap_up(state, block)
+def wrap_up(state, block, close_shutter=False):
+    state, cmd = tel.wrap_up(state, block)
+    if close_shutter:
+        cmd += ["acu.set_shutter(action='close')"]
+    return state, cmd
 
 @cmd.operation(name='lat.ufm_relock', return_duration=True)
 def ufm_relock(state, commands=None, relock_cadence=24*u.hour):
@@ -182,26 +188,27 @@ def move_to(state, az, el, min_el=0, force=False):
 def make_geometry():
     # These are just the median of the wafers and an ~estimated radius
     # To be updated later
-    xi_offset = 0
+    xi_offset = 0.115
+    eta_offset = -0.573
     return {
-        "c1_ws0": {"center": [-0.3710+xi_offset, 0     ], "radius": 0.3,},
-        "c1_ws1": {"center": [ 0.1815+xi_offset, 0.3211], "radius": 0.3,},
-        "c1_ws2": {"center": [ 0.1815+xi_offset,-0.3211], "radius": 0.3,},
-        "i1_ws0": {"center": [-1.9112+xi_offset,-0.9052], "radius": 0.3,},
-        "i1_ws1": {"center": [-1.3584+xi_offset,-0.5704], "radius": 0.3,},
-        "i1_ws2": {"center": [-1.3587+xi_offset,-1.2133], "radius": 0.3,},
-        "i3_ws0": {"center": [ 1.1865+xi_offset,-0.8919], "radius": 0.3,},
-        "i3_ws1": {"center": [ 1.7326+xi_offset,-0.5705], "radius": 0.3,},
-        "i3_ws2": {"center": [ 1.7333+xi_offset,-1.2135], "radius": 0.3,},
-        "i4_ws0": {"center": [ 1.1732+xi_offset, 0.9052], "radius": 0.3,},
-        "i4_ws1": {"center": [ 1.7332+xi_offset, 1.2135], "radius": 0.3,},
-        "i4_ws2": {"center": [ 1.7326+xi_offset, 0.5705], "radius": 0.3,},
-        "i5_ws0": {"center": [-0.3655+xi_offset, 1.7833], "radius": 0.3,},
-        "i5_ws1": {"center": [ 0.1879+xi_offset, 2.1045], "radius": 0.3,},
-        "i5_ws2": {"center": [ 0.1867+xi_offset, 1.4620], "radius": 0.3,},
-        "i6_ws0": {"center": [-1.9082+xi_offset, 0.8920], "radius": 0.3,},
-        "i6_ws1": {"center": [-1.3577+xi_offset, 1.2133], "radius": 0.3,},
-        "i6_ws2": {"center": [-1.3584+xi_offset, 0.5854], "radius": 0.3,},
+        "c1_ws0": {"center": [-0.3710+xi_offset, 0+eta_offset], "radius": 0.3,},
+        "c1_ws1": {"center": [ 0.1815+xi_offset, 0.3211+eta_offset], "radius": 0.3,},
+        "c1_ws2": {"center": [ 0.1815+xi_offset,-0.3211+eta_offset], "radius": 0.3,},
+        "i1_ws0": {"center": [-1.9112+xi_offset,-0.9052+eta_offset], "radius": 0.3,},
+        "i1_ws1": {"center": [-1.3584+xi_offset,-0.5704+eta_offset], "radius": 0.3,},
+        "i1_ws2": {"center": [-1.3587+xi_offset,-1.2133+eta_offset], "radius": 0.3,},
+        "i3_ws0": {"center": [ 1.1865+xi_offset,-0.8919+eta_offset], "radius": 0.3,},
+        "i3_ws1": {"center": [ 1.7326+xi_offset,-0.5705+eta_offset], "radius": 0.3,},
+        "i3_ws2": {"center": [ 1.7333+xi_offset,-1.2135+eta_offset], "radius": 0.3,},
+        "i4_ws0": {"center": [ 1.1732+xi_offset, 0.9052+eta_offset], "radius": 0.3,},
+        "i4_ws1": {"center": [ 1.7332+xi_offset, 1.2135+eta_offset], "radius": 0.3,},
+        "i4_ws2": {"center": [ 1.7326+xi_offset, 0.5705+eta_offset], "radius": 0.3,},
+        "i5_ws0": {"center": [-0.3655+xi_offset, 1.7833+eta_offset], "radius": 0.3,},
+        "i5_ws1": {"center": [ 0.1879+xi_offset, 2.1045+eta_offset], "radius": 0.3,},
+        "i5_ws2": {"center": [ 0.1867+xi_offset, 1.4620+eta_offset], "radius": 0.3,},
+        "i6_ws0": {"center": [-1.9082+xi_offset, 0.8920+eta_offset], "radius": 0.3,},
+        "i6_ws1": {"center": [-1.3577+xi_offset, 1.2133+eta_offset], "radius": 0.3,},
+        "i6_ws2": {"center": [-1.3584+xi_offset, 0.5854+eta_offset], "radius": 0.3,},
     }
 
 def make_cal_target(
@@ -265,12 +272,13 @@ def make_operations(
     iv_cadence=4*u.hour,
     bias_step_cadence=0.5*u.hour,
     det_setup_duration=20*u.minute,
-    home_at_end=False,
+    open_shutter=False,
+    close_shutter=False,
     relock_cadence=24*u.hour
 ):
 
     pre_session_ops = [
-        { 'name': 'lat.preamble'        , 'sched_mode': SchedMode.PreSession},
+        { 'name': 'lat.preamble'        , 'sched_mode': SchedMode.PreSession, 'open_shutter': open_shutter},
         { 'name': 'start_time'          , 'sched_mode': SchedMode.PreSession},
         { 'name': 'set_scan_params'     , 'sched_mode': SchedMode.PreSession, 'az_speed': az_speed, 'az_accel': az_accel, },
     ]
@@ -303,7 +311,7 @@ def make_operations(
     ]
 
     post_session_ops = [
-        { 'name': 'lat.wrap_up'   , 'sched_mode': SchedMode.PostSession},
+        { 'name': 'lat.wrap_up'   , 'sched_mode': SchedMode.PostSession, 'close_shutter': close_shutter},
     ]
 
     return pre_session_ops + cal_ops + cmb_ops + post_session_ops
