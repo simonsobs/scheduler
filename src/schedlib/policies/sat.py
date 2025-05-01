491247c79b3da04ad66df59a8f2dae687d909bb4
mmccrackan <mmccrackan@umass.edu>
2025-05-01 10:34:31 -0400

    fix typo

--- src/schedlib/policies/sat.py
+++ src/schedlib/policies/sat.py
@@ -282,7 +282,7 @@ def bias_step(state, block, bias_step_cadence=None):
 @cmd.operation(name='sat.wiregrid', return_duration=True)
 def wiregrid(state, block, min_wiregrid_el=47.5):
     assert state.hwp_spinning == True, "hwp is not spinning"
-    assert block.alt >= min_wiregrid_el, f"Block {block} is below the minimum wiregrid elevation of {alt_limits[0]} degrees."
+    assert block.alt >= min_wiregrid_el, f"Block {block} is below the minimum wiregrid elevation of {min_wiregrid_el} degrees."
 
     if block.name == 'wiregrid_gain':
         return state, (block.t1 - state.curr_time).total_seconds(), [
