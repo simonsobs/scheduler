bf36c1adb16b845b3dea49f001d536b79eab4ba9
Michael McCrackan <mmccrackan@compute11.simonsobs.org>
2025-05-17 00:21:26 +0000

    prevent duplicate logging for state functions

--- src/schedlib/utils.py
+++ src/schedlib/utils.py
@@ -306,10 +306,11 @@ def init_logger(name):
     logger = logging.getLogger(name)
     logger.propagate = False
     logger.setLevel(logging.INFO)
-    ch = logging.StreamHandler(sys.stdout)
-    formatter = logging.Formatter('%(asctime)s [%(levelname)s] %(message)s ')
-    ch.setFormatter(formatter)
-    logger.addHandler(ch)
+    if not logger.handlers:
+        ch = logging.StreamHandler(sys.stdout)
+        formatter = logging.Formatter('%(asctime)s [%(levelname)s] %(message)s ')
+        ch.setFormatter(formatter)
+        logger.addHandler(ch)
     return logger
 
 def set_logging_level(level=2):
