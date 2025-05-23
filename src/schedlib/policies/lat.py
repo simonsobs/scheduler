156dcbe0b12bd7edcfa2632e481a21e90fd9bd9f
mmccrackan <mmccrackan@umass.edu>
2025-05-23 13:19:37 -0400

    fix typo

--- src/schedlib/policies/lat.py
+++ src/schedlib/policies/lat.py
@@ -653,7 +653,9 @@ class LATPolicy(tel.TelPolicy):
 
                 if self.corotator_override is None:
                     corotator = boresight_to_corotator(cal_target.el_bore, 0)
-                boresight = corotator_to_boresight(cal_target.el_bore, float(corotator))
+                    boresight = corotator_to_boresight(cal_target.el_bore, corotator)
+                else:
+                    boresight = corotator_to_boresight(cal_target.el_bore, float(self.corotator_override))
                 cal_targets[i] = replace(cal_targets[i], boresight_rot=boresight)
 
                 if self.az_branch_override is not None:
