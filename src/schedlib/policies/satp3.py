d9279b295f9387d09eb64e8480ac8b9eda6eafd5
Michael McCrackan <mikemccrackan@gmail.com>
2025-10-15 01:11:51 -0400

    fix satp3 wg det setup (#306)

--- src/schedlib/policies/satp3.py
+++ src/schedlib/policies/satp3.py
@@ -174,7 +174,7 @@ def make_operations(
     wiregrid_ops = []
     if not disable_hwp:
         wiregrid_ops += [
-            { 'name': 'sat.det_setup'       , 'sched_mode': SchedMode.PreWiregrid, 'apply_boresight_rot': apply_boresight_rot, 'iv_cadence':iv_cadence,},
+            { 'name': 'sat.det_setup'       , 'sched_mode': SchedMode.PreWiregrid, 'commands': commands_det_setup, 'apply_boresight_rot': apply_boresight_rot, 'iv_cadence':iv_cadence,},
             { 'name': 'sat.hwp_spin_up'     , 'sched_mode': SchedMode.PreWiregrid, 'disable_hwp': disable_hwp, 'brake_hwp': brake_hwp},
             { 'name': 'sat.wiregrid', 'sched_mode': SchedMode.Wiregrid }
         ]
